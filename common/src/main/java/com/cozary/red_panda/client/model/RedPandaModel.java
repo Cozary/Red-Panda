fdacc0cdac5e013fd950a8a6edc49615022edf20
Cozary <ayrecio99@gmail.com>
2025-01-26 15:55:26 +0100

    Fixed baby model rendering twice

--- common/src/main/java/com/cozary/red_panda/client/model/RedPandaModel.java
+++ common/src/main/java/com/cozary/red_panda/client/model/RedPandaModel.java
@@ -219,7 +219,9 @@ public class RedPandaModel<T extends RedPandaEntity> extends AgeableListModel<T>
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
         super.renderToBuffer(poseStack, buffer, packedLight, packedOverlay, color);
-        body.render(poseStack, buffer, packedLight, packedOverlay, color);
+        if (!this.young) {
+            body.render(poseStack, buffer, packedLight, packedOverlay, color);
+
         head.render(poseStack, buffer, packedLight, packedOverlay, color);
         sleep.render(poseStack, buffer, packedLight, packedOverlay, color);
         frontLeg_R.render(poseStack, buffer, packedLight, packedOverlay, color);
@@ -228,8 +230,8 @@ public class RedPandaModel<T extends RedPandaEntity> extends AgeableListModel<T>
         backLeg_L.render(poseStack, buffer, packedLight, packedOverlay, color);
         tail.render(poseStack, buffer, packedLight, packedOverlay, color);
         shoulder.render(poseStack, buffer, packedLight, packedOverlay, color);
+        }
     }
-
 
     @Override
     protected @NotNull Iterable<ModelPart> headParts() {
